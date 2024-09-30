a419e98cda1088d6e4b1cfbc6a3b7b9e9b6005d0
auniraden <radennn.auni@gmail.com>
2024-09-30 14:31:02 +0800

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Auni Raden on 30/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
